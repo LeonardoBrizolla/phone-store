5d2974e6fe7fb9a4ef318a42afe2866eca16b7a4
LeonardoBrizolla <leonardobrizollars@gmail.com>
2021-12-22 23:25:02 -0300

    Challenge Stars

--- src/description-products.js
+++ src/description-products.js
@@ -1,13 +1,44 @@
 const $heart = document.querySelector('.hearts');
-const $star = document.querySelector('.star');
+const $stars = document.querySelectorAll('.star');
 
 $heart.addEventListener('click', handleClickHeart);
-$star.addEventListener('click', handleClickStar);
+
+$stars.forEach(($star, key) => {
+  if (key === 0) {
+    $star.addEventListener('click', handleClickFistStar);
+  } else if (key > 0 && key < 4) {
+    $star.addEventListener('click', () => handleMiddleStar(key));
+  } else {
+    $star.addEventListener('click', handleLastStar);
+  }
+});
 
 function handleClickHeart() {
-  $heart.classList.toggle('-active');
+  this.classList.toggle('-active');
 }
 
-function handleClickStar() {
-  $star.classList.toggle('-active');
+function handleClickFistStar() {
+  clearAllStars();
+  this.classList.add('-active');
+}
+
+function handleMiddleStar(index) {
+  clearAllStars();
+  $stars.forEach(($star, key) => {
+    if (key <= index) {
+      $star.classList.add('-active');
+    }
+  });
+}
+
+function handleLastStar() {
+  $stars.forEach(($star) => {
+    $star.classList.add('-active');
+  });
+}
+
+function clearAllStars() {
+  $stars.forEach(($star) => {
+    $star.classList.remove('-active');
+  });
 }
